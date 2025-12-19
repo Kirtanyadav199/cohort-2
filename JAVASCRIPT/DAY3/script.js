fed535f784536c5ed0546799840ad81f51a92885
Kirtanyadav199 <kirtanyadav24@gmail.com>
2025-12-19 21:50:59 +0530

    DOM.03

---
+++ JAVASCRIPT/DAY3/script.js
@@ -0,0 +1,14 @@
+//           CREATE ELEMENT
+// let a =document.createElement("h1")
+
+// a.innerHTML = 'Hello Bhai'
+
+// console.log(a);
+
+
+           
+let btn = document.createElement("button")
+let main = document.querySelector('main')
+
+btn.innerHTML = "DOWNLOAD"
+main.appendChild(btn)
